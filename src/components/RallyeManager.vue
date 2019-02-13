a36784d8664e307769592e0fdc6af7898fe2aafb
Alejandro Martinez <alejandro.martinez.0598@gmail.com>
2019-02-13 08:32:44 +0100

    Add path simulated

--- src/components/RallyeManager.vue
+++ src/components/RallyeManager.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-    <h1>{{teams.lenght}} palyers</h1>
+    <h1>{{teams.length}} palyers</h1>
     <div v-for="team in teams" v-bind:key="team.name">
       <h1>{{team.name}}</h1>
       <div v-for="(answer,index) in team.answers" v-bind:key="team.name+index">
@@ -11,7 +11,6 @@
     <button @click="logPlayers()"> refresh</button>
     <div id="mapid" class="mapid"> </div>
   </div>
-  
 </template>
 
 <script>
@@ -19,103 +18,127 @@ import firebase from "firebase";
 
 export default {
   name: 'RallyeManager',
-  components:{
+  components: {
 
   },
   props: {
 
 
   },
-  data(){
-    return{
+  data() {
+    return {
       teams: [],
-      quizzes:[]
+      quizzes: []
     }
   },
-  methods:{
-    logPlayers:function(){
+  methods: {
+    logPlayers: function () {
       console.log(this.teams);
       this.$forceUpdate();
-      
+
     },
-    getAnswersOf:function(teamList){
+    updatePaths: function(){
+        for (let i = 0; i < this.teams.length; i++) {
+          const team = this.teams[i];
+          var path =[]
+          for (let i = 0; i < team.answers.length; i++) {
+            const answer = team.answers[i].id;
+            // TODO put all the location of the answers in a dictionary called an
+            const latLong = this.answers[answer];
+            path.push([latLong.lat,latLong.long]);
+
+
+          }
+         
+        var polyline = L.polyline(path, {color: `${team.color}`}).addTo(map);
+                  
+        }
+    },
+    getAnswersOf: function (teamList) {
       var db = firebase.firestore();
-      
+
       for (let i = 0; i < teamList.length; i++) {
         const team = teamList[i];
         console.log(team.name);
-        
-         db.collection(`Groups/${team.name}/Answers`).get().then((querySnapshot) => {
-           
-            querySnapshot.docs.forEach(element => {
-              if(!team.Answers){
-                    team.answers = [];
-              }
-              var toPush = element.data();
-              toPush.id = element.id;
-              team.answers.push(toPush) 
-            });
-        }) 
+
+        db.collection(`Groups/${team.name}/Answers`).orderBy("startQuiz").get().then((querySnapshot) => {
+
+          querySnapshot.docs.forEach(element => {
+            if (!team.Answers) {
+              team.answers = [];
+            }
+            var toPush = element.data();
+            toPush.id = element.id;
+            team.answers.push(toPush)
+          });
+        })
       }
+
+    },
+    getPlayers: function () {
+      var db = firebase.firestore();
+      var teams = [];
+      db.collection('Groups').get().then((querySnapshot) => {
+
+        querySnapshot.docs.forEach(element => {
+          // the same but reactive this.teams[element.id] = element.data();
+          var toPush = element.data();
+          toPush.name = element.id;
+          this.teams.push(toPush);
+        });
+
+        this.getAnswersOf(this.teams);
+      })
+    },
+    getQuestions: function () {
+      var db = firebase.firestore();
+
+      db.collection('Quizzes').get().then((querySnapshot) => {
+
+        querySnapshot.docs.forEach(element => {
+          this.quizzes[element.id] = element.data()
+
+        });
+
+      })
+
+    },
+    mapInit: function () {
+      var mymap = L.map('mapid', {
+        center: [51.505, -0.09],
+        zoom: 13
+      })
+      // conects the map with the open street maps data
+      this.tileLayer = L.tileLayer(
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 18,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        }
+      );
+      this.tileLayer.addTo(mymap);
+
+      var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+      var circle = L.circle([51.508, -0.11], {
+        color: 'red',
+        fillColor: '#f03',
+        fillOpacity: 0.5,
+        radius: 500
+      }).addTo(mymap);
+
       
     }
 
+
   },
   // on create
-  mounted(){
+  mounted() {
 
-    var mymap = L.map('mapid',{
-      center: [51.505, -0.09],
-      zoom: 13
-    })
-// conects the map with the open street maps data
-this.tileLayer = L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    maxZoom: 18,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-  }
-);
-this.tileLayer.addTo(mymap);
+    this.mapInit()
 
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-var circle = L.circle([51.508, -0.11], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 500
-}).addTo(mymap);
-
-var polygon = L.polygon([
-    [51.509, -0.08],
-    [51.503, -0.06],
-    [51.51, -0.047]
-]).addTo(mymap);
-
-     // gets the data of the teams
-      var db = firebase.firestore();
-        var teams=[];
-        db.collection('Groups').get().then((querySnapshot) => {
-           
-            querySnapshot.docs.forEach(element => {
-             // the same but reactive this.teams[element.id] = element.data();
-              var toPush = element.data();
-              toPush.name = element.id;
-              this.teams.push(toPush);
-            });
-            
-          this.getAnswersOf(this.teams);
-        }) 
-      db.collection('Quizzes').get().then((querySnapshot) => {
-           
-            querySnapshot.docs.forEach(element => {
-              this.quizzes[element.id] = element.data()
-              
-            });
-          
-        }) 
-    
+    // gets the data of the teams
+    this.getPlayers();
+    this.getQuestions();
 
   }
 }
